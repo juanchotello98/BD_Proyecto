590f18157592fddb2ffd121d273057926b5e3e23
juanchotello98 <tello.juan@correounivalle.edu.co>
2017-12-15 02:09:47 -0500

    Modificacion en Lógica: se agrega la clase EnfermeraHabilidades

---
+++ Proyecto_BD/src/Logica/EnfermeraHabilidades.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author Juan David
+ */
+public class EnfermeraHabilidades extends Enfermera {
+    
+    
+    private String habilidad;
+    
+    public EnfermeraHabilidades(String identificacion) {
+        super(identificacion);
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+
+    public void setHabilidad(String habilidad) {
+        this.habilidad = habilidad;
+    }
+  
+}
