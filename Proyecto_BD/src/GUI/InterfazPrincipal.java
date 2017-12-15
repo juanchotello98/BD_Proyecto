81d6a11b8aef2914dda9ce3c1cd1942f9289a913
juanchotello98 <tello.juan@correounivalle.edu.co>
2017-12-15 16:23:30 -0500

    commit para solucionar error

--- Proyecto_BD/src/GUI/InterfazPrincipal.java
+++ Proyecto_BD/src/GUI/InterfazPrincipal.java
@@ -56,6 +56,11 @@ public class InterfazPrincipal extends javax.swing.JFrame {
         );
 
         jButton1.setText("Mi perfil");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Empleados");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -139,6 +144,10 @@ public class InterfazPrincipal extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
