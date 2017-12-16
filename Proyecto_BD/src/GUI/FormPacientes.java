aecd8c55389533976946946fbe817d597333033b
juanchotello98 <tello.juan@correounivalle.edu.co>
2017-12-16 03:09:29 -0500

    x

--- Proyecto_BD/src/GUI/FormPacientes.java
+++ Proyecto_BD/src/GUI/FormPacientes.java
@@ -98,6 +98,8 @@ public class FormPacientes extends javax.swing.JPanel {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
