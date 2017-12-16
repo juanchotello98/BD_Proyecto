a5edb6b97fcac9c054190f1e1398547d32c7a396
juanchotello98 <tello.juan@correounivalle.edu.co>
2017-12-15 23:51:29 -0500

    Solucionar error

--- Proyecto_BD/src/AccesoDatos/FachadaBD.java
+++ Proyecto_BD/src/AccesoDatos/FachadaBD.java
@@ -21,9 +21,9 @@ public class FachadaBD {
     ResultSet tabla;
 
     public FachadaBD() {
-        url="jdbc:postgresql://localhost:5433/bd";
+        url="jdbc:postgresql://localhost:5432/BD_Proyecto";
         usuario = "postgres";
-        password = "3271586+";
+        password = "1628550";
     }
 
     public Connection conectar() {
